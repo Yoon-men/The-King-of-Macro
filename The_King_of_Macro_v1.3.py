ad9e71575228e588d7d2a3ec0a9fa31e2a568e1a
Yoonmen <sgu101310@naver.com>
2021-12-31 10:07:09 +0900

    Simplified structure

--- The_King_of_Macro_v1.3.py
+++ The_King_of_Macro_v1.3.py
@@ -1,5 +1,5 @@
 """
-<The_King_of_Macro_v1.3> - 21.12.30.목 AM
+<The_King_of_Macro_v1.3> - 21.12.31.금 10:06
 * Made by Yoonmen *
 
 [update] 구조 단순화
@@ -136,21 +136,21 @@ class KOM(QWidget) :
         self.addLine.setFrameShadow(QFrame.Sunken)
         
 
-        # deleteMacro_group
-        self.deleteMacro_group = QLabel(Form)
-        self.deleteMacro_group.setGeometry(20, 370, 101, 20)
-        self.deleteMacro_group.setText(_translate("Form", "<Delete Macro>"))
+        # delete_group
+        self.delete_group = QLabel(Form)
+        self.delete_group.setGeometry(20, 370, 101, 20)
+        self.delete_group.setText(_translate("Form", "<Delete Macro>"))
 
-        self.deleteMacro_cb = QComboBox(Form)
-        self.deleteMacro_cb.setGeometry(20, 400, 211, 22)
+        self.delete_cb = QComboBox(Form)
+        self.delete_cb.setGeometry(20, 400, 211, 22)
 
-        self.deleteMacro_lb_1 = QLabel(Form)
-        self.deleteMacro_lb_1.setGeometry(240, 400, 41, 20)
-        self.deleteMacro_lb_1.setText(_translate("Form", "을(를)"))
+        self.delete_lb_1 = QLabel(Form)
+        self.delete_lb_1.setGeometry(240, 400, 41, 20)
+        self.delete_lb_1.setText(_translate("Form", "을(를)"))
 
-        self.deleteMacro_bt = QPushButton(Form)
-        self.deleteMacro_bt.setGeometry(280, 400, 51, 21)
-        self.deleteMacro_bt.setText(_translate("Form", "삭제"))
+        self.delete_bt = QPushButton(Form)
+        self.delete_bt.setGeometry(280, 400, 51, 21)
+        self.delete_bt.setText(_translate("Form", "삭제"))
 
         self.deleteLine = QFrame(Form)
         self.deleteLine.setGeometry(110, 460, 118, 3)
@@ -158,30 +158,217 @@ class KOM(QWidget) :
         self.deleteLine.setFrameShadow(QFrame.Sunken)
 
 
-        # startMacro_group
-        self.startMacro_group = QLabel(Form)
-        self.startMacro_group.setGeometry(20, 480, 101, 20)
-        self.startMacro_group.setText(_translate("Form", "<Start Macro>"))
+        # start_group
+        self.start_group = QLabel(Form)
+        self.start_group.setGeometry(20, 480, 101, 20)
+        self.start_group.setText(_translate("Form", "<Start Macro>"))
 
-        self.startMacro_cb = QComboBox(Form)
-        self.startMacro_cb.setGeometry(20, 510, 141, 22)
+        self.start_cb = QComboBox(Form)
+        self.start_cb.setGeometry(20, 510, 141, 22)
 
-        self.startMacro_lb_1 = QLabel(Form)
-        self.startMacro_lb_1.setGeometry(170, 510, 41, 20)
-        self.startMacro_lb_1.setText(_translate("Form", "을(를)"))
+        self.start_lb_1 = QLabel(Form)
+        self.start_lb_1.setGeometry(170, 510, 41, 20)
+        self.start_lb_1.setText(_translate("Form", "을(를)"))
 
-        self.startMacro_sb = QSpinBox(Form)
-        self.startMacro_sb.setGeometry(210, 510, 42, 22)
+        self.start_sb = QSpinBox(Form)
+        self.start_sb.setGeometry(210, 510, 42, 22)
 
-        self.startMacro_lb_2 = QLabel(Form)
-        self.startMacro_lb_2.setGeometry(260, 510, 16, 20)
-        self.startMacro_lb_2.setText(_translate("Form", "번"))
+        self.start_lb_2 = QLabel(Form)
+        self.start_lb_2.setGeometry(260, 510, 16, 20)
+        self.start_lb_2.setText(_translate("Form", "번"))
 
-        self.startMacro_bt = QPushButton(Form)
-        self.startMacro_bt.setGeometry(280, 510, 51, 21)
-        self.startMacro_bt.setText(_translate("Form", "실행"))
+        self.start_bt = QPushButton(Form)
+        self.start_bt.setGeometry(280, 510, 51, 21)
+        self.start_bt.setText(_translate("Form", "실행"))
 
         QMetaObject.connectSlotsByName(Form)
+
+
+        # If Button is clicked
+        self.loadButton.clicked.connect(self.Load_CSV)
+        self.addName_bt.clicked.connect(self.Add_MACRO)
+        self.addClick_bt.clicked.connect(self.Add_CLICK)
+        self.addKeyboard_bt.clicked.connect(self.Add_KEYBOARD)
+        self.delete_bt.clicked.connect(self.Delete_MACRO)
+        self.start_bt.clicked.connect(self.Start_MACRO)
+
+
+        # When program is started
+        global Load_status
+        Load_status = False
+        self.noticeBoard.addItem("[system] 환영합니다. DATA.csv를 불러와주세요.")
+    
+
+
+    # Function -> Load DATA and Save DATA's Location
+    def Load_CSV(self) : 
+        global Load_status
+        global CSV_road
+        CSV_road = str(QFileDialog.getOpenFileName()[0])
+        CSV_name = os.path.basename(CSV_road)
+        
+        if CSV_name == 'DATA.csv' : 
+            self.noticeBoard.addItem('[system] DATA.csv를 불러오는데 성공했습니다.')
+            # ----------------------------------------------------------------------------------------------------
+            """불러온 CSV 읽기"""
+            global CSV_data
+            CSV_file = open(CSV_road, 'r', encoding = 'utf-8', newline='')
+            CSV_read = csv.reader(CSV_file)
+            CSV_data = []
+
+            for row in CSV_read : 
+                CSV_data.append(row)
+
+            for i in range(1, len(CSV_data)) : 
+                self.addClick_cb.addItem(CSV_data[i][0])
+                self.addKeyboard_cb.addItem(CSV_data[i][0])
+                self.delete_cb.addItem(CSV_data[i][0])
+                self.start_cb.addItem(CSV_data[i][0])
+            
+            Load_status = True         # CSV 파일을 불러온 상태
+            # ----------------------------------------------------------------------------------------------------
+
+        elif CSV_name != 'DATA.csv' : 
+            self.noticeBoard.addItem('[system] DATA.csv를 불러오는데 실패했습니다.')
+
+
+
+    # Function -> Add DATA to CSV
+    def Add_MACRO(self) : 
+        if Load_status == True : 
+            MACRO_name = [self.addName_le.text()]
+            
+            if MACRO_name[0] == '' : 
+                self.noticeBoard.addItem('[system] 공백을 이름으로 사용할 수 없습니다.')
+            else : 
+                self.addClick_cb.addItem(MACRO_name[0])
+                self.addKeyboard_cb.addItem(MACRO_name[0])
+                self.delete_cb.addItem(MACRO_name[0])
+                self.start_cb.addItem(MACRO_name[0])
+
+                CSV_data.append(MACRO_name)
+
+                CSV_file = open(CSV_road, 'w', encoding = 'utf-8', newline='')
+                writer = csv.writer(CSV_file)
+                writer.writerows(CSV_data)
+
+                self.noticeBoard.addItem('[system] 매크로를 추가했습니다.')
+        else : 
+            self.noticeBoard.addItem('[system] 아직 DATA.csv를 불러오지 않았습니다.')
+
+
+
+    # Function -> Add Click's DATA to CSV
+    def Add_CLICK(self) : 
+        if Load_status == True : 
+            object = self.addClick_cb.currentIndex()
+
+            if len(CSV_data[object + 1]) == 1 : 
+                CSV_data[object + 1].append(self.addClick_ds.value())
+
+            else : 
+                CSV_data[object + 1][1] = self.addClick_ds.value()
+
+            while True : 
+                if mouse.is_pressed('left') : 
+                    x, y = pyautogui.position()
+                    CSV_data[object + 1].append((x, y))
+
+                    CSV_file = open(CSV_road, 'w', encoding = 'utf-8', newline='')
+                    writer = csv.writer(CSV_file)
+                    writer.writerows(CSV_data)
+
+                    self.noticeBoard.addItem('[system] 클릭 좌표가 설정되었습니다.')
+                    break
+
+                elif keyboard.is_pressed('ESC') : 
+                    self.noticeBoard.addItem('[system] 클릭 추가가 중지되었습니다.')
+                    break
+        else : 
+            self.noticeBoard.addItem('[system] 아직 DATA.csv를 불러오지 않았습니다.')
+
+
+
+
+    # Function -> Add Keyboard's DATA to CSV
+    def Add_KEYBOARD(self) : 
+        if Load_status == True : 
+            object = self.addKeyboard_cb.currentIndex()
+
+            if len(CSV_data[object + 1]) == 1 : 
+                CSV_data[object + 1].append(self.addKeyboard_ds.value())
+
+            else : 
+                CSV_data[object + 1][1] = self.addKeyboard_ds.value()
+            
+            key = keyboard.read_hotkey(suppress=False)
+            CSV_data[object + 1].append(key)
+
+            CSV_file = open(CSV_road, 'w', encoding = 'utf-8', newline='')
+            writer = csv.writer(CSV_file)
+            writer.writerows(CSV_data)
+
+            self.noticeBoard.addItem('[system] 키보드 입력이 설정되었습니다.')
+
+        else : 
+            self.noticeBoard.addItem('[system] 아직 DATA.csv를 불러오지 않았습니다.')
+
+
+
+    # Function -> Delete Macro's DATA
+    def Delete_MACRO(self) : 
+        if Load_status == True : 
+            object = self.delete_cb.currentIndex()
+
+            self.addClick_cb.removeItem(object)
+            self.addKeyboard_cb.removeItem(object)
+            self.start_cb.removeItem(object)
+            self.delete_cb.removeItem(object)
+            del CSV_data[object + 1]
+
+            CSV_file = open(CSV_road, 'w', encoding = 'utf-8', newline='')
+            writer = csv.writer(CSV_file)
+            writer.writerows(CSV_data)
+
+            self.noticeBoard.addItem('[system] 매크로를 삭제했습니다.')
+        else : 
+            self.noticeBoard.addItem('[system] 아직 DATA.csv를 불러오지 않았습니다.')
+
+
+
+    # Function -> Start Macro
+    def Start_MACRO(self) : 
+        if Load_status == True :
+            object = self.start_cb.currentIndex()
+            delay = float(CSV_data[object + 1][1])
+            macro_Num = len(CSV_data[object + 1]) - 2
+            for i in range(self.start_sb.value()) : 
+                for j in range(macro_Num) : 
+                    if str(type(CSV_data[object + 1][j + 2])) == "<class 'str'>" :          # 가공되지 않은 좌표 or 키보드 입력
+                        """좌표 가공"""
+                        CSV_data[object+1][j+2] = CSV_data[object+1][j+2].strip('(')
+                        CSV_data[object+1][j+2] = CSV_data[object+1][j+2].strip(')')
+                        CSV_data[object+1][j+2] = CSV_data[object+1][j+2].split(', ')
+
+                        if str(type(CSV_data[object+1][j+2])) == "<class 'list'>" :         # 가공된 좌표일 경우
+                            pyautogui.moveTo(CSV_data[object + 1][j + 2])
+                            time.sleep(0.05)
+                            pyautogui.click(CSV_data[object + 1][j + 2])
+                            time.sleep(delay)
+                        else :      # 키보드 입력일 경우
+                            pyautogui.press(CSV_data[object + 1][j + 2])
+                            time.sleep(delay)
+
+                    else :      # 방금 막 저장한 매크로의 경우
+                        pyautogui.moveTo(CSV_data[object + 1][j + 2])
+                        time.sleep(0.05)
+                        pyautogui.click(CSV_data[object + 1][j + 2])
+                        time.sleep(delay)
+            self.noticeBoard.addItem('[system] 매크로 작동이 완료되었습니다.')
+
+        else : 
+            self.noticeBoard.addItem('[system] 아직 DATA.csv를 불러오지 않았습니다.')
+
 
 
 
