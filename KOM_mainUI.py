824b13fbcefe3d5cd7b623ff85a680350515afb9
Yoonmen <sgu101310@naver.com>
2022-07-30 22:37:50 +0900

    mainUI For KOM

--- KOM_mainUI.py
+++ KOM_mainUI.py
@@ -106,7 +106,7 @@ class MainUI(QMainWindow) :
         self.noticeBoard = QListWidget(self.body_frm)
         self.noticeBoard.setGeometry(18, 529, 370, 118)
         self.noticeBoard.setFocusPolicy(Qt.NoFocus)
-        self.noticeBoard.setFont(QFont("나눔고딕", 9, QFont.ExtraBold))
+        self.noticeBoard.setFont(QFont("나눔고딕OTF", 9, QFont.ExtraBold))
         self.noticeBoard.setStyleSheet("QListWidget{\n"
                                             "background-color : #4d4d4d;\n"
                                             "border-radius : 8px;\n"
@@ -133,7 +133,7 @@ class MainUI(QMainWindow) :
         #addName_part
         self.addName_lb_title = QLabel(self.body_frm)
         self.addName_lb_title.setGeometry(20, 76, 151, 21)
-        self.addName_lb_title.setFont(QFont("나눔고딕", 12, QFont.ExtraBold))
+        self.addName_lb_title.setFont(QFont("나눔고딕OTF", 12, QFont.ExtraBold))
         self.addName_lb_title.setStyleSheet("QLabel{\n"
                                                 "color : #b1b1b1;\n"
                                             "}")
@@ -141,7 +141,7 @@ class MainUI(QMainWindow) :
 
         self.addName_le = QLineEdit(self.body_frm)
         self.addName_le.setGeometry(20, 107, 300, 24)
-        self.addName_le.setFont(QFont("나눔고딕", 10, QFont.ExtraBold))
+        self.addName_le.setFont(QFont("나눔고딕OTF", 10, QFont.ExtraBold))
         self.addName_le.setStyleSheet("QLineEdit{\n"
                                             "color : #dddddd;\n"
                                             "background-color : #303030;\n"
@@ -156,7 +156,7 @@ class MainUI(QMainWindow) :
 
         self.addName_bt = QPushButton(self.body_frm)
         self.addName_bt.setGeometry(328, 107, 60, 24)
-        self.addName_bt.setFont(QFont("나눔고딕", 9, QFont.ExtraBold))
+        self.addName_bt.setFont(QFont("나눔고딕OTF", 9, QFont.ExtraBold))
         self.addName_bt.setStyleSheet("QPushButton{\n"
                                             "border : 2px solid #aaaaaa;\n"
                                             "border-radius : 8px;\n"
@@ -172,7 +172,7 @@ class MainUI(QMainWindow) :
         # edit_part
         self.edit_lb_title = QLabel(self.body_frm)
         self.edit_lb_title.setGeometry(20, 193, 81, 21)
-        self.edit_lb_title.setFont(QFont("나눔고딕", 12, QFont.ExtraBold))
+        self.edit_lb_title.setFont(QFont("나눔고딕OTF", 12, QFont.ExtraBold))
         self.edit_lb_title.setStyleSheet("QLabel{\n"
                                             "color : #b1b1b1;\n"
                                             "}")
@@ -180,7 +180,7 @@ class MainUI(QMainWindow) :
 
         self.edit_bt = QPushButton(self.body_frm)
         self.edit_bt.setGeometry(20, 227, 369, 24)
-        self.edit_bt.setFont(QFont("나눔고딕", 9, QFont.ExtraBold))
+        self.edit_bt.setFont(QFont("나눔고딕OTF", 9, QFont.ExtraBold))
         self.edit_bt_inactive()
         self.edit_bt.setText("편집")
 
@@ -188,7 +188,7 @@ class MainUI(QMainWindow) :
         # delete_part
         self.delete_lb_title = QLabel(self.body_frm)
         self.delete_lb_title.setGeometry(20, 313, 101, 21)
-        self.delete_lb_title.setFont(QFont("나눔고딕", 12, QFont.ExtraBold))
+        self.delete_lb_title.setFont(QFont("나눔고딕OTF", 12, QFont.ExtraBold))
         self.delete_lb_title.setStyleSheet("QLabel{\n"
                                                 "color : #b1b1b1;\n"
                                             "}")
@@ -197,7 +197,7 @@ class MainUI(QMainWindow) :
         self.delete_cb = QComboBox(self.body_frm)
         self.delete_cb.setGeometry(20, 347, 300, 24)
         self.delete_cb.setStyleSheet("QComboBox{\n"
-                                        "font-family : 나눔고딕;\n"
+                                        "font-family : 나눔고딕OTF;\n"
                                         "font-weight : bold;\n"
                                         "font-size : 10pt;\n"
                                         "border-radius : 5px;\n"
@@ -224,7 +224,7 @@ class MainUI(QMainWindow) :
 
         self.delete_bt = QPushButton(self.body_frm)
         self.delete_bt.setGeometry(328, 347, 60, 24)
-        self.delete_bt.setFont(QFont("나눔고딕", 9, QFont.ExtraBold))
+        self.delete_bt.setFont(QFont("나눔고딕OTF", 9, QFont.ExtraBold))
         self.delete_bt.setStyleSheet("QPushButton{\n"
                                         "border : 2px solid #aaaaaa;\n"
                                         "border-radius : 8px;\n"
@@ -240,7 +240,7 @@ class MainUI(QMainWindow) :
         # start_part
         self.start_lb_title = QLabel(self.body_frm)
         self.start_lb_title.setGeometry(20, 442, 101, 21)
-        self.start_lb_title.setFont(QFont("나눔고딕", 12, QFont.ExtraBold))
+        self.start_lb_title.setFont(QFont("나눔고딕OTF", 12, QFont.ExtraBold))
         self.start_lb_title.setStyleSheet("QLabel{\n"
                                             "color : #b1b1b1;\n"
                                         "}")
@@ -255,7 +255,7 @@ class MainUI(QMainWindow) :
 
         self.start_rb_typeNum = QRadioButton(self.start_frm)
         self.start_rb_typeNum.setGeometry(6, 0, 91, 21)
-        self.start_rb_typeNum.setFont(QFont("나눔고딕", 10, QFont.ExtraBold))
+        self.start_rb_typeNum.setFont(QFont("나눔고딕OTF", 10, QFont.ExtraBold))
         self.start_rb_typeNum.setStyleSheet("QRadioButton{\n"
                                                 "color : #dddddd;\n"
                                             "}")
@@ -264,7 +264,7 @@ class MainUI(QMainWindow) :
 
         self.start_rb_typeTime = QRadioButton(self.start_frm)
         self.start_rb_typeTime.setGeometry(102, 0, 91, 21)
-        self.start_rb_typeTime.setFont(QFont("나눔고딕", 10, QFont.ExtraBold))
+        self.start_rb_typeTime.setFont(QFont("나눔고딕OTF", 10, QFont.ExtraBold))
         self.start_rb_typeTime.setStyleSheet("QRadioButton{\n"
                                                 "color : #dddddd;\n"
                                             "}")
@@ -273,7 +273,7 @@ class MainUI(QMainWindow) :
         self.start_cb = QComboBox(self.body_frm)
         self.start_cb.setGeometry(20, 477, 163, 24)
         self.start_cb.setStyleSheet("QComboBox{\n"
-                                        "font-family : 나눔고딕;\n"
+                                        "font-family : 나눔고딕OTF;\n"
                                         "font-weight : bold;\n"
                                         "font-size : 10pt;\n"
                                         "border-radius : 5px;\n"
@@ -299,7 +299,7 @@ class MainUI(QMainWindow) :
 
         self.start_lb_1 = QLabel(self.body_frm)
         self.start_lb_1.setGeometry(192, 477, 41, 21)
-        self.start_lb_1.setFont(QFont("나눔고딕", 10, QFont.ExtraBold))
+        self.start_lb_1.setFont(QFont("나눔고딕OTF", 10, QFont.ExtraBold))
         self.start_lb_1.setStyleSheet("QLabel{\n"
                                             "color : #b1b1b1;\n"
                                         "}")
@@ -307,7 +307,7 @@ class MainUI(QMainWindow) :
 
         self.start_le = QLineEdit(self.body_frm)
         self.start_le.setGeometry(235, 479, 60, 22)
-        self.start_le.setFont(QFont("나눔고딕", 10, QFont.ExtraBold))
+        self.start_le.setFont(QFont("나눔고딕OTF", 10, QFont.ExtraBold))
         self.start_le.setStyleSheet("QLineEdit{\n"
                                             "color : #dddddd;\n"
                                             "background-color : #303030;\n"
@@ -325,7 +325,7 @@ class MainUI(QMainWindow) :
 
         self.start_lb_2 = QLabel(self.body_frm)
         self.start_lb_2.setGeometry(304, 477, 16, 21)
-        self.start_lb_2.setFont(QFont("나눔고딕", 10, QFont.ExtraBold))
+        self.start_lb_2.setFont(QFont("나눔고딕OTF", 10, QFont.ExtraBold))
         self.start_lb_2.setStyleSheet("QLabel{\n"
                                     "color : #b1b1b1;\n"
                                 "}")
@@ -333,13 +333,13 @@ class MainUI(QMainWindow) :
 
         self.start_bt_typeNum = QPushButton(self.body_frm)
         self.start_bt_typeNum.setGeometry(328, 477, 60, 24)
-        self.start_bt_typeNum.setFont(QFont("나눔고딕", 9, QFont.ExtraBold))
+        self.start_bt_typeNum.setFont(QFont("나눔고딕OTF", 9, QFont.ExtraBold))
         self.start_bt_typeNum_inactive()
         self.start_bt_typeNum.setText("실행")
 
         self.start_bt_typeTime = QPushButton(self.body_frm)
         self.start_bt_typeTime.setGeometry(328, 477, 60, 24)
-        self.start_bt_typeTime.setFont(QFont("나눔고딕", 9, QFont.ExtraBold))
+        self.start_bt_typeTime.setFont(QFont("나눔고딕OTF", 9, QFont.ExtraBold))
         self.start_bt_typeTime_inactive()
         self.start_bt_typeTime.setText("실행")
         self.start_bt_typeTime.hide()
