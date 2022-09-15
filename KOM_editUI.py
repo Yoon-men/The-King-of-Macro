95579b81887c979bd509ea78a923cbcc1f2f377a
Yoonmen <sgu101310@naver.com>
2022-09-16 00:24:28 +0900

    editUI For KOM

--- KOM_editUI.py
+++ KOM_editUI.py
@@ -4,6 +4,8 @@ from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 from PySide2.QtCore import *
 
+from The_King_of_Macro import palette
+
 class EditUI(QDialog) : 
     def __init__(self) : 
         super().__init__()
