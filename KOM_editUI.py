b71b91744bb865841fefd9f2f84e3a432c25dc33
Yoonmen <sgu101310@naver.com>
2023-03-05 17:45:29 +0900

    editUI For KOM

--- KOM_editUI.py
+++ KOM_editUI.py
@@ -1,6 +1,6 @@
 from img.img import *
 import sys
-from PySide2.QtWidgets import QApplication, QDialog, QFrame, QGraphicsDropShadowEffect, QLabel, QPushButton, QComboBox, QListWidget, QDoubleSpinBox, QLineEdit
+from PySide2.QtWidgets import QApplication, QDialog, QFrame, QGraphicsDropShadowEffect, QLabel, QPushButton, QComboBox, QListWidget, QDoubleSpinBox, QLineEdit, QRadioButton
 from PySide2.QtCore import Qt, QSize
 from PySide2.QtGui import QIcon, QFontDatabase, QFont, QIntValidator
 
@@ -447,6 +447,177 @@ class AddColorCheckerUI(QDialog) :
         self.y_coordinate_le.setStyleSheet(le_styleSheet)
         self.y_coordinate_le.setValidator(QIntValidator())
         self.y_coordinate_le.setAlignment(Qt.AlignCenter)
+
+        line_styleSheet = ("QLabel{\n"
+                                "image : url(:/img/line.png);\n"
+                            "}")
+        
+        self.line_1 = QLabel(self.body_frm)
+        self.line_1.setGeometry(53, 170, 241, 16)
+        self.line_1.setStyleSheet(line_styleSheet)
+
+
+        # palette_part
+        palette_styleSheet = ("QRadioButton::indicator{\n"
+                                    "width : 46px;\n"
+                                    "height : 21px;\n"
+                                    "background-color : #ff00ff;\n"
+                                    "border-radius : 6px;\n"
+                                "}\n"
+                                "QRadioButton::indicator::checked{\n"
+                                    "border : 2px solid #ffffff;\n"
+                                "}")
+        
+        self.palette_rb_1 = QRadioButton(self.body_frm)
+        self.palette_rb_1.setGeometry(61, 214, 50, 25)
+        self.palette_rb_1.setStyleSheet(palette_styleSheet)
+        self.palette_rb_1.setChecked(True)
+
+        self.palette_rb_2 = QRadioButton(self.body_frm)
+        self.palette_rb_2.setGeometry(120, 214, 50, 25)
+        self.palette_rb_2.setStyleSheet(palette_styleSheet)
+        self.palette_rb_2.hide()
+
+        self.palette_rb_3 = QRadioButton(self.body_frm)
+        self.palette_rb_3.setGeometry(179, 214, 50, 25)
+        self.palette_rb_3.setStyleSheet(palette_styleSheet)
+        self.palette_rb_3.hide()
+
+        self.palette_rb_4 = QRadioButton(self.body_frm)
+        self.palette_rb_4.setGeometry(238, 214, 50, 25)
+        self.palette_rb_4.setStyleSheet(palette_styleSheet)
+        self.palette_rb_4.hide()
+
+        self.palette_rb_5 = QRadioButton(self.body_frm)
+        self.palette_rb_5.setGeometry(61, 248, 50, 25)
+        self.palette_rb_5.setStyleSheet(palette_styleSheet)
+        self.palette_rb_5.hide()
+
+        self.palette_rb_6 = QRadioButton(self.body_frm)
+        self.palette_rb_6.setGeometry(120, 248, 50, 25)
+        self.palette_rb_6.setStyleSheet(palette_styleSheet)
+        self.palette_rb_6.hide()
+
+        self.palette_rb_7 = QRadioButton(self.body_frm)
+        self.palette_rb_7.setGeometry(179, 248, 50, 25)
+        self.palette_rb_7.setStyleSheet(palette_styleSheet)
+        self.palette_rb_7.hide()
+
+        self.palette_rb_8 = QRadioButton(self.body_frm)
+        self.palette_rb_8.setGeometry(238, 248, 50, 25)
+        self.palette_rb_8.setStyleSheet(palette_styleSheet)
+        self.palette_rb_8.hide()
+
+        self.palette_rb_9 = QRadioButton(self.body_frm)
+        self.palette_rb_9.setGeometry(61, 282, 50, 25)
+        self.palette_rb_9.setStyleSheet(palette_styleSheet)
+        self.palette_rb_9.hide()
+
+        self.palette_rb_10 = QRadioButton(self.body_frm)
+        self.palette_rb_10.setGeometry(120, 282, 50, 25)
+        self.palette_rb_10.setStyleSheet(palette_styleSheet)
+        self.palette_rb_10.hide()
+
+        self.palette_rb_11 = QRadioButton(self.body_frm)
+        self.palette_rb_11.setGeometry(179, 282, 50, 25)
+        self.palette_rb_11.setStyleSheet(palette_styleSheet)
+        self.palette_rb_11.hide()
+
+        self.palette_rb_12 = QRadioButton(self.body_frm)
+        self.palette_rb_12.setGeometry(238, 282, 50, 25)
+        self.palette_rb_12.setStyleSheet(palette_styleSheet)
+        self.palette_rb_12.hide()
+
+        self.addPalette_bt = QPushButton(self.body_frm)
+        self.addPalette_bt.setGeometry(133, 216, 21, 21)
+        self.addPalette_bt.setStyleSheet("QPushButton{\n"
+                                            "image : url(:/img/bt_plus_normal.png);\n"
+                                            "border-radius : 10px;\n"
+                                        "}\n"
+                                        "QPushButton:hover{\n"
+                                            "image : url(:/img/bt_plus_hover.png);\n"
+                                        "}")
+        
+        self.R_lb = QLabel(self.body_frm)
+        self.R_lb.setGeometry(72, 341, 13, 21)
+        self.R_lb.setFont(QFont("나눔고딕OTF", 12, QFont.Bold))
+        self.R_lb.setStyleSheet(lb_styleSheet)
+        self.R_lb.setText("R")
+
+        self.R_le = QLineEdit(self.body_frm)
+        self.R_le.setGeometry(92, 339, 50, 24)
+        self.R_le.setFont(QFont("나눔고딕OTF", 10, QFont.Bold))
+        self.R_le.setStyleSheet(le_styleSheet)
+        self.R_le.setValidator(QIntValidator())
+        self.R_le.setAlignment(Qt.AlignCenter)
+        self.R_le.setText("255")
+
+        self.G_lb = QLabel(self.body_frm)
+        self.G_lb.setGeometry(70, 374, 20, 21)
+        self.G_lb.setFont(QFont("나눔고딕OTF", 10, QFont.Bold))
+        self.G_lb.setStyleSheet(lb_styleSheet)
+        self.G_lb.setText("G")
+
+        self.G_le = QLineEdit(self.body_frm)
+        self.G_le.setGeometry(92, 372, 50, 24)
+        self.G_le.setFont(QFont("나눔고딕OTF", 10, QFont.Bold))
+        self.G_le.setStyleSheet(le_styleSheet)
+        self.G_le.setValidator(QIntValidator())
+        self.R_le.setAlignment(Qt.AlignCenter)
+        self.R_le.setText("0")
+
+        self.B_lb = QLabel(self.body_frm)
+        self.B_lb.setGeometry(72, 407, 20, 21)
+        self.B_lb.setFont(QFont("나눔고딕OTF", 12, QFont.Bold))
+        self.B_lb.setStyleSheet(lb_styleSheet)
+        self.B_lb.setText("B")
+
+        self.B_le = QLineEdit(self.body_frm)
+        self.B_le.setGeometry(92, 405, 50, 24)
+        self.B_le.setFont(QFont("나눔고딕OTF", 10, QFont.Bold))
+        self.B_le.setStyleSheet(le_styleSheet)
+        self.B_le.setValidator(QIntValidator())
+        self.B_le.setAlignment(Qt.AlignCenter)
+        self.B_le.setText("255")
+
+        bt_styleSheet = ("QPushButton{\n"
+                            "background-color : #202020;\n"
+                            "border : 2px solid #aaaaaa;\n"
+                            "border-radius : 5px;\n"
+                            "color : #cccccc;\n"
+                        "}\n"
+                        "QPushButton:hover{\n"
+                            "background-color : #aaaaaa;\n"
+                            "color : #222222;\n"
+                        "}")
+
+        self.copyColor_bt = QPushButton(self.body_frm)
+        self.copyColor_bt.setGeometry(162, 339, 111, 91)
+        self.copyColor_bt.setFont(QFont("나눔고딕OTF", 9, QFont.Bold))
+        self.copyColor_bt.setStyleSheet(bt_styleSheet)
+        self.copyColor_bt.setText("좌표 컬러 복사")
+
+        self.line_2 = QLabel(self.body_frm)
+        self.line_2.setGeometry(53, 457, 241, 16)
+        self.line_2.setStyleSheet(line_styleSheet)
+        
+
+        # colorCheckerProcess_part
+        self.checkingDelay_le = QLineEdit(self.body_frm)
+        self.checkingDelay_le.setGeometry(80, 504, 60, 22)
+        self.checkingDelay_le.setFont(QFont("나눔고딕OTF", 10, QFont.Bold))
+        self.checkingDelay_le.setStyleSheet(le_styleSheet)
+        self.checkingDelay_le.setValidator(QIntValidator())
+        self.checkingDelay_le.setAlignment(Qt.AlignCenter)
+        self.checkingDelay_le.setText("0")
+
+        self.checkingDelay_lb = QLabel(self.body_frm)
+        self.checkingDelay_lb.setGeometry(149, 505, 121, 21)
+        self.checkingDelay_lb.setFont(QFont("나눔고딕OTF", 10, QFont.Bold))
+        self.checkingDelay_lb.setStyleSheet(lb_styleSheet)
+        self.checkingDelay_lb.setText("초마다 컬러체크 진행")
+
+
     
 
 
