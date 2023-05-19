b889d32c4fab09fa00a4c9a0986e71b9181257bd
Yoonmen <yoonmen03@gmail.com>
2023-05-19 15:56:30 +0900

    settingUI For KOM

--- KOM_settingUI.py
+++ KOM_settingUI.py
@@ -78,7 +78,7 @@ class SettingUI(QDialog) :
         self.setWindowTitle("Setting")
         icon_path = path.join(path.dirname(__file__), "KOM.ico")
         if path.isfile(icon_path) : 
-            self.setWindowIcon(icon_path)
+            self.setWindowIcon(QIcon(icon_path))
         font_path = path.join(path.dirname(__file__), "NanumGothicBold.otf")
         if path.isfile(font_path) : 
             QFontDatabase.addApplicationFont(font_path)
